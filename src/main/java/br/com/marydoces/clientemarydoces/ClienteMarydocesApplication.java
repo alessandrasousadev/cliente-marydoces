241ce2eb674e023670f337b5ef2f430633e8c2eb
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-02-27 11:58:16 -0300

    inicia o projeto

---
+++ src/main/java/br/com/marydoces/clientemarydoces/ClienteMarydocesApplication.java
@@ -0,0 +1,13 @@
+package br.com.marydoces.clientemarydoces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteMarydocesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteMarydocesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marydoces/clientemarydoces/ClienteMarydocesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marydoces.clientemarydoces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteMarydocesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
