7da9a0a1d77c413c5ee7df5e1e0c1025deb62a2d
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-07-05 23:06:28 -0300

    implementa dominio de negocio

--- src/main/java/br/com/marydoces/clientemarydoces/cliente/application/domain/Cliente.java
+++ src/main/java/br/com/marydoces/clientemarydoces/cliente/application/domain/Cliente.java
@@ -1,5 +1,56 @@
 package br.com.marydoces.clientemarydoces.cliente.application.domain;
 
-public class Cliente {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "idcliente", updatable = false, unique = true, nullable = false)
+	private UUID idCliente;
+	@NotBlank
+	private String nomeCompleto;
+	@CPF
+	@Column(unique = true)
+	private String cpf;
+	@NotBlank
+	private String endereco;
+	@NotBlank
+	private String celular;
+	@NotNull
+	private LocalDate dataNascimento;
+	@NotNull
+	private Boolean aceitaTermos;
+
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
+	
+	public Cliente(UUID idCliente, @NotBlank String nomeCompleto, @CPF String cpf, @NotBlank String endereco,
+			@NotBlank String celular, @NotNull LocalDate dataNascimento, @NotNull Boolean aceitaTermos) {
+		this.idCliente = UUID.randomUUID();
+		this.nomeCompleto = nomeCompleto;
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.celular = celular;
+		this.dataNascimento = dataNascimento;
+		this.aceitaTermos = aceitaTermos;
+		this.dataHoraDoCadastro = LocalDateTime.now();	
+	}	
 }
