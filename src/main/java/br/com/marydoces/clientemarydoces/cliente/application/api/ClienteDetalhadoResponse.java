04537b55f727be3bdd8326e441bff632b61fe9d8
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-07-14 00:05:13 -0300

    implementa fluxo de infraRepository

--- src/main/java/br/com/marydoces/clientemarydoces/cliente/application/api/ClienteDetalhadoResponse.java
+++ src/main/java/br/com/marydoces/clientemarydoces/cliente/application/api/ClienteDetalhadoResponse.java
@@ -4,10 +4,12 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import br.com.marydoces.clientemarydoces.cliente.application.domain.Cliente;
 import lombok.Value;
 
 @Value
 public class ClienteDetalhadoResponse {
+	
 	private UUID idCliente;
 	private String nomeCompleto;
 	private String cpf;
@@ -16,4 +18,15 @@ public class ClienteDetalhadoResponse {
 	private LocalDate dataNascimento;
 	private Boolean aceitaTermos;
 	private LocalDateTime dataHoraDoCadastro;
+	
+	public ClienteDetalhadoResponse(Cliente cliente) {
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.cpf = cliente.getCpf();
+		this.endereco = cliente.getEndereco();
+		this.celular = cliente.getCelular();
+		this.dataNascimento = cliente.getDataNascimento();
+		this.aceitaTermos = cliente.getAceitaTermos();
+		this.dataHoraDoCadastro = cliente.getDataHoraDoCadastro();
+	}
 }
--- src/main/java/br/com/marydoces/clientemarydoces/cliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/marydoces/clientemarydoces/cliente/application/repository/ClienteRepository.java
@@ -1,11 +1,12 @@
 package br.com.marydoces.clientemarydoces.cliente.application.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import br.com.marydoces.clientemarydoces.cliente.application.domain.Cliente;
 
 public interface ClienteRepository {
 	Cliente salva(Cliente cliente);
 	List<Cliente> buscaTodosClientes();
-
+	Cliente buscaClienteAtravesId(UUID idCliente);
 }
