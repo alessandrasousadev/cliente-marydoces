9b9a63d2e4a14a2e45cdbd6956cb3495e9e22214
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-08-02 22:08:07 -0300

    implementa conversao no listresponse

--- src/main/java/br/com/marydoces/clientemarydoces/pedido/application/api/PedidoClienteListResponse.java
+++ src/main/java/br/com/marydoces/clientemarydoces/pedido/application/api/PedidoClienteListResponse.java
@@ -1,14 +1,28 @@
 package br.com.marydoces.clientemarydoces.pedido.application.api;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+import br.com.marydoces.clientemarydoces.pedido.domain.Doce;
 import br.com.marydoces.clientemarydoces.pedido.domain.Pedido;
+import lombok.Value;
 
+@Value
 public class PedidoClienteListResponse {
-
+	private UUID idPedido;
+	private Doce doce;
+	private int quantidade;
+	
 	public static List<PedidoClienteListResponse> converte(List<Pedido> pedidosDoCliente) {
-		
-		return null;
+		return pedidosDoCliente.stream()
+				.map(PedidoClienteListResponse::new)
+				.collect(Collectors.toList());
 	}
-
+	
+	public PedidoClienteListResponse(Pedido pedido) {
+		this.idPedido = pedido.getIdClientePedido();
+		this.doce = pedido.getDoce();
+		this.quantidade = pedido.getQuantidade();
+	}
 }
