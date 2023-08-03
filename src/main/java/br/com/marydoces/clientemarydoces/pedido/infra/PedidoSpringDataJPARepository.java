e943cf055a3aa9b71b87cfef6324ed5e7b3fe664
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-08-02 21:56:29 -0300

    implementa infra repository busca pedido do cliente por id

--- src/main/java/br/com/marydoces/clientemarydoces/pedido/infra/PedidoSpringDataJPARepository.java
+++ src/main/java/br/com/marydoces/clientemarydoces/pedido/infra/PedidoSpringDataJPARepository.java
@@ -1,5 +1,6 @@
 package br.com.marydoces.clientemarydoces.pedido.infra;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.marydoces.clientemarydoces.pedido.domain.Pedido;
 
 public interface PedidoSpringDataJPARepository  extends JpaRepository<Pedido, UUID>{
-
+	List<Pedido> findByIdClientePedido(UUID idClientePedido);
 }
